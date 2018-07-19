ef5b2fca41a35b658834b2d6c6dc521ad2a6dcee
SeungGu Jun <jsg3001kr@daum.net>
2018-07-19 23:35:12 +0900

    플레이어 상태 구현 수정
    점프가 이동보다 우선순위를 더 높임
    //함수분리

--- Assets/02.Scripts/Gerneral/State.cs
+++ Assets/02.Scripts/Gerneral/State.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum STATE { STAND, JUMP }
+enum STATE { STAND, JUMP, WALK, RUN }
 public class State  {
 
 	
--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -12,11 +12,11 @@ public class Player : MonoBehaviour {
     private Jump jump;
     public float rotSpeed = 250.0f; //회전 속도
 
-
+    [SerializeField]
     private STATE eState = STATE.STAND;
     private bool isDoubleJump = false;
 
-    public bool npcTalk = false;
+  
  
     void Start()
     {
@@ -38,6 +38,7 @@ public class Player : MonoBehaviour {
         playerTr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r); // Y축을 기준으로 rotSpeed 만큼 회전
 
         KeyboardManual();
+        MoveManual();
     }
 
 
@@ -50,22 +51,25 @@ public class Player : MonoBehaviour {
                 playerRb.AddForce(new Vector3(0, 1.6f, 0) * 5.0f, ForceMode.Impulse);
                 isDoubleJump = false;
             }
-            else if(eState == STATE.STAND)
+            else if(eState != STATE.JUMP)
             {
                 eState = STATE.JUMP;
                 jump.Action(1.6f, 5.0f);     //점프력,점프스피드
                 isDoubleJump = true;
             }
         }
+       
         if(Input.GetKeyDown(KeyCode.Return))
         {
          //뭐를쓸까낭?
         }
+     
+
     }
 
     private void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "Ground")
+        if (col.gameObject.tag == "Ground" && eState == STATE.JUMP)
         {
             eState = STATE.STAND;
             isDoubleJump = false;
@@ -77,5 +81,18 @@ public class Player : MonoBehaviour {
         
     }
 
+    private void MoveManual()
+    {
+        if (eState == STATE.JUMP) return;
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            eState = STATE.WALK;
+        }
+        else
+        {
+             eState = STATE.STAND;
+
+        }
+    }
 }
  
