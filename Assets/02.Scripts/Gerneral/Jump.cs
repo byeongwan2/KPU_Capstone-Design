dfbce66bf4e816c47799976e78d301de0fa4f0de
SeungGu Jun <jsg3001kr@daum.net>
2018-07-19 20:01:16 +0900

    점프구현
    플레이어는 점프가 가능

---
+++ Assets/02.Scripts/Gerneral/Jump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    [SerializeField]
+    private float jumpForce = 1.6f;
+    [SerializeField]
+    private float jumpSpeed = 5f;
+
+    private bool isJumping = false;
+
+    private Rigidbody this_rigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        this_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        if (isJumping)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this_rigidbody.AddForce(new Vector3(0, jumpForce, 0) * jumpSpeed, ForceMode.Impulse); //위방향으로 올라가게함
+                isJumping = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+            isJumping = true;
+    }
+}
--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -6,13 +6,14 @@ public class Player : MonoBehaviour {
     private float r = 0.0f;
 
     private Transform playerTr;
+    
 
     public float rotSpeed = 250.0f; //회전 속도
-                                    // Use this for initialization
+   
     void Start()
     {
-
         playerTr = GetComponent<Transform>(); //Player Transform 컴포넌트 할당
+            
     }
 
 
@@ -21,6 +22,10 @@ public class Player : MonoBehaviour {
         r = Input.GetAxis("Mouse X");
 
         playerTr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r); // Y축을 기준으로 rotSpeed 만큼 회전
+
+        
     }
 
+   
 }
+ 
