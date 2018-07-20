792fff26728c08a68eb00f4d37a139c9f52b20aa
SeungGu Jun <jsg3001kr@daum.net>
2018-07-20 15:47:51 +0900

    그냥구현
    ㅇㅇ

--- Assets/02.Scripts/Gerneral/Check.cs
+++ Assets/02.Scripts/Gerneral/Check.cs
@@ -8,4 +8,9 @@ public class Check  {
     {
         return Vector3.Distance(_a.position, _b.position);
     }
+
+    public static float Distance(GameObject _a,GameObject _b)       //그냥심심해서만든 오버로딩 ㅋㅋ
+    {
+        return Vector3.Distance(_a.transform.position, _b.transform.position);
+    }
 }
