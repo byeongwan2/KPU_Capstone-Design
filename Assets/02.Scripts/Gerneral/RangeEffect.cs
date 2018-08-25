15ae3013ab504b73c1ca03bfc86fa6b3863a43c2
SeungGu Jun <jsg3001kr@daum.net>
2018-08-25 19:21:32 +0900

    폭탄범위 이펙트 하다가말음
    짜쯩..;;

---
+++ Assets/02.Scripts/Gerneral/RangeEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeEffect : MonoBehaviour
+{
+    LineRenderer m_line;
+
+    void Start()
+    {
+        m_line = GetComponent<LineRenderer>();
+        m_line.SetVertexCount(10);
+        m_line.useWorldSpace = false;
+        RangeMaker();
+    }
+    void RangeMaker()
+    {
+        float x, y,z = 0.0f;
+        float angle = 20.0f;
+
+        for(int i = 0; i < 10; i++)
+        {
+            x = Mathf.Cos(Mathf.Deg2Rad * angle) * 10.0f;
+            y = Mathf.Sin(Mathf.Deg2Rad * angle) * 10.0f;
+
+            m_line.SetPosition(i, new Vector3(x, y, z));
+            angle += (360.0f / 9);
+        }
+    }
+
+}
