e3459c08fd99eb4bec5d0f859ffdba51bed56c36
jsg <jsg3001kr@daum.net>
2019-03-18 23:03:04 +0900

    오브젝트 전부킨상태

--- Assets/02.Scripts/Stage/Stage01.cs
+++ Assets/02.Scripts/Stage/Stage01.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum CHAPTER { ONE,TWO}
+enum CHAPTER { ONE,TWO,THREE}
 public class Stage01 : MonoBehaviour {
 
     GameSystem system;
     RezenCore rezenCore;
-    GameObject []mapObject = new GameObject[1000];
+    GameObject []mapObject = new GameObject[3000];
     CHAPTER nowChapter = CHAPTER.ONE;
 	void Start () {
         //rezenCore = GetComponent<RezenCore>();
@@ -22,28 +22,39 @@ public class Stage01 : MonoBehaviour {
     {
         foreach (var obj in mapObject)
         {
-            if(obj.transform.position.y > system.pPlayer2.transform.position.y)
-            {
-                obj.SetActive(false);
-            }
-            else
+            
+            if(obj.transform.position.y < system.pPlayer2.transform.position.y + 3 && obj.transform.position.y > system.pPlayer2.transform.position.y -3)
             {
                 obj.SetActive(true);
             }
-            
+            else
+            {
+                obj.SetActive(false);
+            }
+            if(obj.transform.position.y == 0)
+            {
+                obj.SetActive(true);
+            }
         }
-
-
     }
+
+    
 
     void Update()
     {
        switch (nowChapter)
         {
             case CHAPTER.ONE:
-                if(30 < system.pPlayer2.transform.position.x)
+                if(-4.1 < system.pPlayer2.transform.position.y)
                 {
                     nowChapter = CHAPTER.TWO;
+                    Culling_Sight_Height();
+                }
+                break;
+            case CHAPTER.TWO:
+                if(-0.1 < system.pPlayer2.transform.position.y)
+                {
+                    nowChapter = CHAPTER.THREE;
                     Culling_Sight_Height();
                 }
                 break;
