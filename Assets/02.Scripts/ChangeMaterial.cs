91ae7acd2ee71270cac3ce304fe3ee61296d697b
KimSatGat <cbaal@korea.com>
2019-05-10 17:36:55 +0900

    ChangeMaterial, CheckMonster 스크립트 추가
    CheckMonster : 마우스가 Monster 감지
    ChangeMaterial : Monster 감지시 Outline Material로 변경

---
+++ Assets/02.Scripts/ChangeMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    Material origin_mt;     // 기존 재질
+    Material outLine_mt;    // Outline 재질
+    new Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        // 재질 할당
+        origin_mt = renderer.material;
+        outLine_mt = Resources.Load("OutLine", typeof(Material)) as Material;
+        
+    }    
+
+    public void SetOutLine()
+    {
+        // 재질 변경
+        renderer.material = outLine_mt;
+    }
+    
+    public void SetOrigin()
+    {
+        // 재질 변경
+        renderer.material = origin_mt;
+    }
+}
---
+++ Assets/02.Scripts/CheckMonster.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 마우스로 적을 감별하여 외곽선 셰이더를 구현하는 스크립트
+
+public class CheckMonster : MonoBehaviour
+{
+    [SerializeField]    // 디버깅을 위해서
+    public GameObject target;
+    private ChangeMaterial cm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GetObject();
+        if (target == null)
+        {
+            return;
+        }
+        // 적을 감지하면
+        if(target.CompareTag("Enemy"))
+        {
+            // 적의 ChangeMaterial 컴포넌트를 가지고 온다
+            cm = target.GetComponentInChildren<ChangeMaterial>();
+            // 외곽선 셰이더 설정
+            cm.SetOutLine();            
+        }
+        else
+        {
+            if(cm != null)
+            {
+                cm.SetOrigin();
+            }
+        }
+    }
+
+    private GameObject GetObject()
+    {
+        RaycastHit hit;
+        GameObject target = null;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    // 마우스 포인터 좌표 생성
+
+        if((Physics.Raycast(ray.origin, ray.direction * 10, out hit)) == true)
+        {
+            target = hit.collider.gameObject;
+            return target;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
